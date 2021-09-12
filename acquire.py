102a68434c89650a75a1cabc7bc9d249c6f324b6
jamesallen0351 <james.allen0351@gmail.com>
2021-09-12 13:23:15 -0500

    add aquire functions

--- acquire.py
+++ acquire.py
@@ -15,9 +15,56 @@ from env import host, user, password # I already added my env to .gitignore firs
 
 # establishing get_connection function
 def get_connection(db, user=user, host=host, password=password):
-    '''
-    This function gets my info from my env file and creats a connection url 
-    
-    '''
+
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
 
+
+    
+def new_zillow_data():
+    sql_query = '''select * from properties_2017
+    join predictions_2017 using(parcelid)
+    where transactiondate between "2017-05-01" and "2017-08-31"
+    and propertylandusetypeid in (260, 261, 262, 263, 264, 265, 266, 273, 275, 276, 279)'''
+    
+    df = pd.read_sql(sql_query, get_connection('zillow'))
+    
+    return df 
+
+
+
+def get_zillow_data():
+    if os.path.isfile('zillow.csv'):
+        df = pd.read_csv('zillow.csv', index_col=0)
+    
+    else:
+        df = new_zillow_data()
+        df.to_csv('zillow.csv')
+    
+    return df
+    
+    
+    
+    
+def clean_zillow(df):
+    #select features for df, took these features from my acquire exercise
+    features = ['parcelid', 'calculatedfinishedsquarefeet', 'bathroomcnt', 'bedroomcnt', 'taxvaluedollarcnt','yearbuilt','taxamount','fips']
+    df = df[features]
+
+    #rename columns for easier use
+    df = df.rename(columns={
+                            'parcelid': 'parcel_id',
+                            'calculatedfinishedsquarefeet': 'sqft',
+                            'bathroomcnt': 'baths',
+                            'bedroomcnt': 'beds',
+                            'taxvaluedollarcnt':'tax_value',
+                            'yearbuilt':'year_built',
+                            'taxamount': 'tax_amount'
+        
+    })
+    
+    #set index
+    df = df.set_index('parcel_id')
+    #drop nulls
+    df = df.dropna(subset=['sqft','tax_value'])
+    
+    return df
