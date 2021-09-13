09b1187f775bace887c77ddb468b2d94a1b3fe84
jamesallen0351 <james.allen0351@gmail.com>
2021-09-13 12:21:07 -0500

    add prepare.py with functions in progress

---
+++ prepare.py
@@ -0,0 +1,181 @@
+# prepare file 
+
+import os
+import pandas as pd
+import numpy as np
+from scipy import stats
+from env import username, host, password 
+from sklearn.feature_selection import SelectKBest, f_regression, RFE
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.linear_model import LinearRegression
+
+
+def remove_outlier(df):
+
+    new_df = df[(np.abs(stats.zscore(df['sqft'])) < 3)]
+    new_df = df[(np.abs(stats.zscore(df['baths'])) < 3)]
+    new_df = df[(np.abs(stats.zscore(df['beds'])) < 3)]
+    new_df = df[(np.abs(stats.zscore(df['tax_value'])) < 3)]
+    
+    return new_df
+    
+def clean_zillow(df):
+
+    #select features for df
+    features = ['parcelid', 'calculatedfinishedsquarefeet', 'bathroomcnt', 'bedroomcnt', 'taxvaluedollarcnt', 'yearbuilt','fips']
+    df = df[features]
+    
+    #for the yearbuilt column, fill in nulls with 2017.
+    df['yearbuilt'].fillna(2017, inplace = True)
+   
+    #create a new column named 'age', which is 2017 minus the yearbuilt
+    df['age'] = 2017-df['yearbuilt']
+    
+    #drop duplicates in parcelid
+    df = df.drop_duplicates(subset=['parcelid'])
+    
+    #rename columns for easier use
+    df = df.rename(columns={
+                            'parcelid': 'parcel_id',
+                            'calculatedfinishedsquarefeet': 'sqft',
+                            'bathroomcnt': 'baths',
+                            'bedroomcnt': 'beds',
+                            'taxvaluedollarcnt':'tax_value'
+        
+    })
+    
+    #set index
+    df = df.set_index('parcel_id')
+    
+    #drop nulls in sqft and tax_value
+    df = df.dropna(subset=['sqft','tax_value'])
+    
+    #drop year_built, we can just use age.
+    df = df.drop(columns=['yearbuilt'])
+    
+    return df
+    
+
+
+def train_validate_test(df, target):
+  
+    # split df into test (20%) and train_validate (80%)
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123)
+
+    # split train_validate off into train (70% of 80% = 56%) and validate (30% of 80% = 24%)
+    train, validate = train_test_split(train_validate, test_size=.3, random_state=123)
+
+        
+    # split train into X (dataframe, drop target) & y (series, keep target only)
+    X_train = train.drop(columns=[target])
+    y_train = train[target]
+    
+    # split validate into X (dataframe, drop target) & y (series, keep target only)
+    X_validate = validate.drop(columns=[target])
+    y_validate = validate[target]
+    
+    # split test into X (dataframe, drop target) & y (series, keep target only)
+    X_test = test.drop(columns=[target])
+    y_test = test[target]
+    
+    return train, validate, test, X_train, y_train, X_validate, y_validate, X_test, y_test
+
+
+def get_object_cols(df):
+  
+    # create a mask of columns whether they are object type or not
+    mask = np.array(df.dtypes == "object")
+
+        
+    # get a list of the column names that are objects (from the mask)
+    object_cols = df.iloc[:, mask].columns.tolist()
+    
+    return object_cols
+
+def get_numeric_X_cols(X_train, object_cols):
+ 
+    numeric_cols = [col for col in X_train.columns.values if col not in object_cols]
+    
+    return numeric_cols
+
+def min_max_scale(X_train, X_validate, X_test, numeric_cols):
+ 
+    # create the scaler object and fit it to X_train (i.e. identify min and max)
+    # if copy = false, inplace row normalization happens and avoids a copy (if the input is already a numpy array).
+
+
+    scaler = MinMaxScaler(copy=True).fit(X_train[numeric_cols])
+
+    #scale X_train, X_validate, X_test using the mins and maxes stored in the scaler derived from X_train.  
+    X_train_scaled_array = scaler.transform(X_train[numeric_cols])
+    X_validate_scaled_array = scaler.transform(X_validate[numeric_cols])
+    X_test_scaled_array = scaler.transform(X_test[numeric_cols])
+
+    # convert arrays to dataframes
+    X_train_scaled = pd.DataFrame(X_train_scaled_array, 
+                                  columns=numeric_cols).\
+                                  set_index([X_train.index.values])
+
+    X_validate_scaled = pd.DataFrame(X_validate_scaled_array, 
+                                     columns=numeric_cols).\
+                                     set_index([X_validate.index.values])
+
+    X_test_scaled = pd.DataFrame(X_test_scaled_array, 
+                                 columns=numeric_cols).\
+                                 set_index([X_test.index.values])
+
+    
+    return X_train_scaled, X_validate_scaled, X_test_scaled
+
+    
+
+def clean_zillow_taxes(df):
+   
+    #select features for df
+    features = ['parcelid', 'calculatedfinishedsquarefeet', 'bathroomcnt', 'bedroomcnt', 'taxvaluedollarcnt', 'yearbuilt','fips', 'taxamount']
+    df = df[features]
+    
+    #fill in nulls of year_built with 2017
+    df['yearbuilt'].fillna(2017, inplace = True)
+    
+    #calculate age by subtracting yearbuilt from 2017
+    df['age'] = 2017-df['yearbuilt']
+   
+    #calculate tax_rate by having taxamount divided by taxvaluedollarcnt
+    df['tax_rate'] = df['taxamount'] / df['taxvaluedollarcnt']
+    
+    #drop duplicates in parcelid
+    df = df.drop_duplicates(subset=['parcelid'])
+    
+    #rename columns for easier use
+    df = df.rename(columns={
+                            'parcelid': 'parcel_id',
+                            'calculatedfinishedsquarefeet': 'sqft',
+                            'bathroomcnt': 'baths',
+                            'bedroomcnt': 'beds',
+                            'taxvaluedollarcnt':'tax_value',
+                            'taxamount': 'tax_amount'
+        
+    })
+    
+    #set index
+    df = df.set_index('parcel_id')
+    
+    #drop nulls
+    df = df.dropna(subset=['sqft','tax_value', 'tax_amount'])
+    
+    #drop year_built
+    df = df.drop(columns=['yearbuilt'])
+    
+    return df
+
+def remove_outlier_tax(df):
+  
+    new_df = df[(np.abs(stats.zscore(df['sqft'])) < 3)]
+    new_df = df[(np.abs(stats.zscore(df['baths'])) < 3)]
+    new_df = df[(np.abs(stats.zscore(df['beds'])) < 3)]
+    new_df = df[(np.abs(stats.zscore(df['tax_value'])) < 3)]
+    new_df = df[(np.abs(stats.zscore(df['tax_amount'])) < 3)]
+    
+    return new_df
